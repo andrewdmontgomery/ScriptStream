66d0c6ac7b0a9fa1026cb3bff1013e11a75231fb
Andrew D. Montgomery <andrewdmontgomery@mac.com>
2012-08-21 12:41:46 -0400

    Add replica preflight/create

--- scriptStream.sh
+++ scriptStream.sh
@@ -15,8 +15,19 @@
 # Start/Stop Password Service
 /usr/sbin/slapconfig -startpasswordserver
 /usr/sbin/slapconfig -stoppasswordserver
+
+## OD Master
 # Preflight for OD Master
 /usr/sbin/slapconfig -preflightmaster --certAuthName "Acme, Inc. Open Directory Certification Authority" --certAdminEmail "support@example.com" acmediradmin 1000
 
 # Create OD Master
 /usr/sbin/slapconfig -createldapmasterandadmin --certAuthName "Acme, Inc. Open Directory Certification Authority" --certAdminEmail "support@example.com" --certOrgName "Acme, Inc." acmediradmin "Acme LDAP-Admin" 1000 "dc=corp,dc=acme,dc=com" CORP.ACME.COM
+
+## OD Replica
+# Preflight for OD Replica
+# Note: the order of the directory admin and the master's hostname is reversed from the 'create' command
+/usr/sbin/slapconfig -preflightreplica acmediradmin odr.example.com
+
+# Create OD Replica
+# Note: the order of the directory admin and the master's hostname is reversed from the 'preflight' command
+/usr/sbin/slapconfig -createreplica --certAdminEmail "support@example.com" odr.example.com acmediradmin
